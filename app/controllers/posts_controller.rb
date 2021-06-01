5a7133590f5ad4b408ecee19b15f73617c50794e
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-01 10:40:05 +0900

    詳細画面へのリンクの設定とビュー（HTML）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,6 +16,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
   private
   def post_params
     params.require(:post).permit(:image,:class_name, :detail, :grade_id, :subject_id, :unit_id).merge(user_id: current_user.id)
