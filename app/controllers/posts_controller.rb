e01433f3f3f2c522f293a0e08d9fa4ea76a6e55c
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-01 12:01:13 +0900

    投稿詳細ページ・投稿編集ページのビューの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    unless user_signed_in? && current_user.id == @post.user_id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:image,:class_name, :detail, :grade_id, :subject_id, :unit_id).merge(user_id: current_user.id)
