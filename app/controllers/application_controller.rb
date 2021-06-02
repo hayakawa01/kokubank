742ce38e62a273420deddf817187210d46eed36f
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-02 14:25:35 +0900

    Basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameter,if: :devise_controller?
+  before_action :basic_auth
 
 
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
+
   def configure_permitted_parameter
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:first_name, :family_name, :first_name_kana, :family_name_kana, :favorite_subject_id, :prefecture_id, :career_id, :introduction])
   end
