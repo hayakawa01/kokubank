9b4c3408e511715efa2e02aa6c0d85f3804a8ea9
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-08 11:34:26 +0900

    モデルの記述が消えていたので復元

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :grade
+  has_many :comments, dependent: :destroy
+  has_one_attached :image
+
+  
+  with_options presence: true do
+    validates :class_name,length:{ maximum: 40}
+    validates :detail,length:{ maximum: 1000}
+    validates :image
+    #with_options numericality: {other_than: 1} do
+      #validates :grade_id
+    #end
+  end
+
 end
