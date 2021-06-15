ad8addaae16a81779c7594ddaab605c2fcf37006
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-15 15:25:38 +0900

    ビューの実装

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -13,4 +13,8 @@ module NotificationsHelper
         tag.a(@visiter.nickname, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_path(notification.post_id), style:"font-weight: bold;")+"にコメントしました"
     end
   end
+
+  def unchecked_notifications
+    @notifications = current_user.passive_notifications.where(checked: false)
+  end
 end
