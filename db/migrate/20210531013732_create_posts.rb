df92040732883593a5b4511e0e586302330977d6
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-19 20:04:14 +0900

    migrationファイルを直接直してしまっていたので、もとに復元

--- db/migrate/20210531013732_create_posts.rb
+++ db/migrate/20210531013732_create_posts.rb
@@ -3,7 +3,9 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string      :class_name, null:false
       t.text        :detail,     null:false
-      t.references  :grade,      foreign_key: true
+      t.integer     :grade_id,   null: false
+      t.integer     :subject_id, null: false
+      t.integer     :unit_id,    null: false
       t.references  :user,       foreign_key: true
       t.timestamps
     end
