a807c8354c4fb7199443146dd762e78f721e64f0
clo-kinng <83395366+clo-kinng@users.noreply.github.com>
2021-06-21 12:17:26 +0900

    修正

---
+++ db/migrate/20210619115603_add_columns_to_posts.rb
@@ -0,0 +1,4 @@
+class AddColumnsToPosts < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_15_003918) do
+ActiveRecord::Schema.define(version: 2021_06_19_115603) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
